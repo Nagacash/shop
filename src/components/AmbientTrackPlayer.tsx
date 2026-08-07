17708422e944742cffc91797c5ed3a93570fd2b5
Moe <chosenfewrecords@hotmail.de>
2026-08-07 14:35:41 +0200

    new2

--- src/components/AmbientTrackPlayer.tsx
+++ src/components/AmbientTrackPlayer.tsx
@@ -153,7 +153,6 @@ export default function AmbientTrackPlayer() {
         preload="none"
         aria-hidden="true"
         controlsList="nodownload noplaybackrate"
-        disablePictureInPicture
         onContextMenu={(event) => event.preventDefault()}
       />
 
